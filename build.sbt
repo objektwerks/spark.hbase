b21bc1d285c1a53479eb6570dd33cef84fac9c98
objektwerks <objektwerks@runbox.com>
2019-03-12 16:31:59 -0400

    added hbase table builder and loader

--- build.sbt
+++ build.sbt
@@ -4,11 +4,15 @@ version := "0.1"
 scalaVersion := "2.12.8"
 libraryDependencies ++= {
   val sparkVersion = "2.4.0"
+  val hbaseVersion = "2.1.3"
   Seq(
     "org.apache.spark" %% "spark-core" % sparkVersion,
     "org.apache.spark" %% "spark-streaming" % sparkVersion,
     "org.apache.spark" %% "spark-sql" % sparkVersion,
-    "org.apache.hbase" % "hbase-client" % "2.1.3",
+    "org.apache.hbase" % "hbase-client" % hbaseVersion,
+    "org.apache.hbase" % "hbase-server" % hbaseVersion,
+    "org.apache.hbase" % "hbase-common" % hbaseVersion,
+    "org.apache.hadoop" % "hadoop-common" % "3.2.0",
     "com.typesafe" % "config" % "1.3.3",
     "org.scalatest" %% "scalatest" % "3.0.5" % Test
   )
--- src/main/scala/spark/SparkHBaseApp.scala
+++ src/main/scala/spark/SparkHBaseApp.scala
@@ -1,15 +1,20 @@
 package spark
 
 import com.typesafe.config.ConfigFactory
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.ConnectionFactory
 import org.apache.spark.sql.SparkSession
 
 object SparkHBaseApp extends App {
-  val conf = ConfigFactory.load("app.conf").getConfig("app")
+  val appConf = ConfigFactory.load("app.conf").getConfig("app")
+  val hbaseConf = HBaseConfiguration.create
+  val connection = ConnectionFactory.createConnection(hbaseConf)
+  val admin =  connection.getAdmin
 
   val sparkSession = SparkSession
     .builder
-    .master(conf.getString("master"))
-    .appName(conf.getString("name"))
+    .master(appConf.getString("master"))
+    .appName(appConf.getString("name"))
     .getOrCreate()
 
   sys.addShutdownHook {
