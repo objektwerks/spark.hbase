64c6fc4feed17c3f31abc006829f61b88348ab54
objektwerks <objektwerks@runbox.com>
2019-03-13 16:15:57 -0400

    refactored hbase proxy values method and app caller code

--- src/main/scala/spark/SparkHBaseApp.scala
+++ src/main/scala/spark/SparkHBaseApp.scala
@@ -9,22 +9,34 @@ object SparkHBaseApp extends App {
   val conf = ConfigFactory.load("app.conf")
 
   val hbaseProxy = HBaseProxy(conf)
-  val hbaseValues = hbaseProxy.values.getOrElse(Seq.empty[String])
+  hbaseProxy.values.fold(
+    throwable => exit(throwable),
+    values => run(values)
+  )
 
-  val master = conf.getString("spark.master")
-  val app = conf.getString("spark.app")
-  val sparkSession = SparkSession.builder
-    .master(master)
-    .appName(app)
-    .getOrCreate()
-  log.info(s"*** Created Spark session for app: $app")
-
-  sys.addShutdownHook {
-    sparkSession.stop()
-    log.info(s"*** Stopped Spark session for app: $app")
+  def exit(throwable: Throwable): Unit = {
+    log.error(throwable)
+    System.exit(-1)
   }
 
-  import sparkSession.implicits._
+  def run(values: Seq[String]): Unit = {
+    val master = conf.getString("spark.master")
+    val app = conf.getString("spark.app")
+    val sparkSession = SparkSession.builder
+      .master(master)
+      .appName(app)
+      .getOrCreate()
+    log.info(s"*** Created Spark session for app: $app")
 
-  val dataset = sparkSession.createDataset(hbaseValues)
+    sys.addShutdownHook {
+      sparkSession.stop()
+      log.info(s"*** Stopped Spark session for app: $app")
+    }
+
+    import sparkSession.implicits._
+
+    val dataset = sparkSession.createDataset(values)
+    dataset.show()
+  }
+
 }
