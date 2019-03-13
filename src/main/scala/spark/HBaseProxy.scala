aa36fa17d09a59716bfd6e511f3ebe2e472ff42e
objektwerks <objektwerks@runbox.com>
2019-03-13 09:57:53 -0400

    merged hbase builder and loader into hbase proxy; edited app conf

---
+++ src/main/scala/spark/HBaseProxy.scala
@@ -0,0 +1,43 @@
+package spark
+
+import com.typesafe.config.Config
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
+import org.apache.hadoop.hbase.client._
+import org.apache.hadoop.hbase.util.Bytes
+
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.JavaConverters._
+
+class HBaseProxy(conf: Config) {
+  val hbaseConf = HBaseConfiguration.create
+  val connection = ConnectionFactory.createConnection(hbaseConf)
+  val admin =  connection.getAdmin
+  val tableName = conf.getString("hbase.tablename")
+  val columnFamily = conf.getString("hbase.columnfamily")
+
+  def build: Unit = {
+    val table = TableName.valueOf(tableName)
+    val column = ColumnFamilyDescriptorBuilder.of(columnFamily)
+    val descripter = TableDescriptorBuilder.newBuilder(table).setColumnFamily(column).build()
+    admin.createTable(descripter)
+  }
+
+  def put(count: Int): Unit = {
+    val puts = ArrayBuffer.empty[Put]
+    for (i <- 1 to count) {
+      val columnFamilyAsBytes = columnFamily.getBytes
+      val valueAsString = i.toString
+      val valueAsBytes = valueAsString.getBytes
+      val row = Bytes.toBytes(valueAsString)
+      val put = new Put(row)
+      put.addColumn(columnFamilyAsBytes, valueAsBytes, valueAsBytes)
+      puts += put
+    }
+    val table = connection.getTable(TableName.valueOf(tableName))
+    table.put(puts.asJava)
+  }
+}
+
+object HBaseProxy {
+  def apply(conf: Config): HBaseProxy = new HBaseProxy(conf)
+}
--- src/main/scala/spark/SparkHBaseApp.scala
+++ src/main/scala/spark/SparkHBaseApp.scala
@@ -1,20 +1,16 @@
 package spark
 
 import com.typesafe.config.ConfigFactory
-import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.hbase.client.ConnectionFactory
 import org.apache.spark.sql.SparkSession
 
 object SparkHBaseApp extends App {
-  val appConf = ConfigFactory.load("app.conf").getConfig("app")
-  val hbaseConf = HBaseConfiguration.create
-  val connection = ConnectionFactory.createConnection(hbaseConf)
-  val admin =  connection.getAdmin
+  val conf = ConfigFactory.load("app.conf")
+  val hbaseProxy = HBaseProxy(conf)
 
   val sparkSession = SparkSession
     .builder
-    .master(appConf.getString("master"))
-    .appName(appConf.getString("name"))
+    .master(conf.getString("master"))
+    .appName(conf.getString("name"))
     .getOrCreate()
 
   sys.addShutdownHook {
